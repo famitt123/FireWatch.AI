423e2c94fa48f3dc20d09f0cacc9de776da14354
Divit Asrani <divit.asrani@gmail.com>
2026-07-21 14:02:07 -0400

    Fixed JSON list for fire_incidents, included titles with each row, here is sample data (not real): [{'fire_number':'KEN-51','status':'out of control','size_hectares':4120.0}]

--- backend/main.py
+++ backend/main.py
@@ -3,7 +3,7 @@ import psycopg2 # importing the psycopg2 library to let python send sql commands
 
 app = FastAPI()
 
-# This function basically opens a live connection to Postgres using the correct address and login, and gives that connection back to whoever asked for it.
+# This function basically opens a live connection to Postgres using the correct address and login and gives that connection back to whoever asked for it.
 # Basic clinet to server and server to clinet relationship :D
 def get_connection():
     return psycopg2.connect(
@@ -14,17 +14,27 @@ def get_connection():
         password="devpassword"
     )
 
-#Created an endpoint which connects to Postgres (Main database), runs a SELECT on fire_incidents and returns the results as JSON
+#Created an endpoint which connects to Postgres (Main database), runs a SELECT on fire_incidents relational model and returns the results as JSON
 @app.get('/fires')
 def get_fires():
     conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute("SELECT fire_number, status, size_hectares FROM fire_incidents;")
-    rows = cursor.fetchall()
-    cursor.close()
-    conn.close()
-    return rows
-    
+    cursor = conn.cursor() # get a cursor to send SQL and read results
+    cursor.execute("SELECT fire_number, status, size_hectares FROM fire_incidents;") # excutes the actual query that I have selected
+    rows = cursor.fetchall() # This command fetches all of the data from the matching rows (like from the SELECT statement above :D) of the fire_incidents query
+  
+    # Convert each unlabeled database row into a labeled dictionary so the JSON output is clear
+    fires = []
+    for row in rows:
+        fires.append({
+            "fire_number": row[0],
+            "status": row[1],
+            "size_hectares": row[2]
+        })
+    cursor.close() # closes cursor to free up space on the server
+    conn.close()  # closes connection completely
+    return fires
+
+
 @app.get('/')
 def read_root():
     return {'message': 'FireWatch.AI backend server is Running :D'}
